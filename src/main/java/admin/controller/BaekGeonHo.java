31f975c10008b1ee78629799cc1552f4a0d7ef01
BGH0827 <pedov46@naver.com>
2021-09-27 18:13:17 +0900

    수정

---
+++ src/main/java/admin/controller/BaekGeonHo.java
@@ -0,0 +1,14 @@
+package admin.controller;
+
+public class BaekGeonHo {
+
+	public BaekGeonHo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 백건호
+	}
+
+}
